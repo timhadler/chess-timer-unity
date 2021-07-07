fc47ebe5192a9c6b13961680682d337d52896fc5
Tim Hadler <timhadler93@gmail.com>
2021-07-07 21:47:41 +1200

    Pushing existing repo

---
+++ Assets/Button.cs
@@ -0,0 +1,90 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public Manager managerScript;
+    public TMP_Text timerText;
+    public Color activeColor;
+    public bool active = false;        // Player clock active
+
+    private Image image;
+    private float timerValue = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.color = Color.grey;
+        DisplayTimer(timerValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active && managerScript.gameActive)
+        {
+            if (timerValue > 0)
+            {
+                timerValue -= Time.deltaTime;
+            }
+            else
+            {
+                timerValue = 0;
+                image.color = Color.red;
+                active = false;
+            }
+
+            DisplayTimer(timerValue);
+        }
+    }
+
+
+    public void ToggleActive()
+    {
+        if (managerScript.gameActive)
+        {
+            if (active)
+            {
+                setDisabled();
+
+            }
+            else
+            {
+                setActive();
+            }
+        }
+    }
+
+
+    public void setActive()
+    {
+        image.color = activeColor;
+        active = true;
+    }
+
+
+    void setDisabled()
+    {
+        image.color = Color.grey;
+        active = false;
+    }
+
+
+    void DisplayTimer(float _time)
+    {
+        float min;
+        float sec;
+
+        if (_time < 0)
+        {
+            _time = 0;
+        }
+
+        min = Mathf.FloorToInt(_time / 60);
+        sec = Mathf.FloorToInt(_time % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", min, sec);
+    }
+}
---
+++ Assets/Manager.cs
@@ -0,0 +1,64 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class Manager : MonoBehaviour
+{
+    public bool gameActive = false;
+    public GameObject player_1;
+    public GameObject player_2;
+    public TMP_Text startPauseText;
+
+    private bool gameHasStarted = false;
+    private Button playerScript_1;
+    private Button playerScript_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript_1 = player_1.GetComponent<Button>();
+        playerScript_2 = player_2.GetComponent<Button>();
+    }
+
+    public void StartPause()
+    {
+        if (gameActive)
+        {
+            startPauseText.text = "Start";
+            gameActive = false;
+        }
+        else
+        {
+            startPauseText.text = "Pause";
+            gameActive = true;
+
+            if (!gameHasStarted)
+            {
+                playerScript_1.ToggleActive();
+                gameHasStarted = true;
+            }
+        }
+    }
+
+    public void Player_1_Button()
+    {
+        if (playerScript_1.active)
+        {
+            SwitchTurn();
+        }
+    }
+
+    public void Player_2_Button()
+    {
+        if (playerScript_2.active)
+        {
+            SwitchTurn();
+        }
+    }
+
+    void SwitchTurn()
+    {
+        playerScript_1.ToggleActive();
+        playerScript_2.ToggleActive();
+    }
+}
